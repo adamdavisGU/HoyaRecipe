c229048f2e90563a7a8395e865dcd87a1c5f3a5d
gditroliobravo <ged33@georgetown.edu>
2021-05-05 16:10:46 -0400

    1

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ button.addEventListener("click",()=>{
 
 //fetch data from the API
 async function sendApiRequest() {
+  let APP_ID = "433cf146"
+  let API_key = "ab1a5c06888a216223f12a09f1922bf6"	
   let response = await fetch(``)
+  console.log(response)
 };
 
 
